3e1a91461ffcbeeb4dbc14f509f01be0c81ce2a6
Adrián Lorenzo Melián <adrianlorenzomelian@gmail.com>
2020-07-05 17:11:58 +0100

    Change argparser descriptions.

--- utils/args.py
+++ utils/args.py
@@ -2,12 +2,14 @@ import argparse
 
 
 def buildArgumentParser():
-    argparser = argparse.ArgumentParser()
+    argparser = argparse.ArgumentParser(description="""
+    Implementation with CLI interface of the work done for the degree final project "Combinación de apariencia e información de contexto para la re-identificación en competiciones deportivas".
+    """)
 
-    argparser.add_argument("--silent", action='store_false', help="Runs program in silent mode (no verbose).")
+    argparser.add_argument("--silent", action='store_false', help="Runs the program in silent mode (no verbose).")
 
     argparser.add_argument("--embedding", type=str, metavar="image_file",
-                           help="Returns embedding of the given body. Default embedding generator: AlignedReId++")
+                           help="Returns embedding of the given body. Default embedding generator: AlignedReId++.")
     argparser.add_argument("--alignedreid", action='store_true',
                            help="Uses AlignedReId++ for body embedding generation.")
     argparser.add_argument("--abd", action='store_true',
@@ -16,10 +18,10 @@ def buildArgumentParser():
                            help="Embedding generator weights file.")
     argparser.add_argument("--embeddingGeneratorClasses", type=int, metavar="classes_number",
                            help="Embedding generator number of classes.")
-    argparser.add_argument("--savePath", type=str, metavar="save_embedding_path", help="Embedding save path (use .h5 extension)")
+    argparser.add_argument("--savePath", type=str, metavar="save_embedding_path", help="Embedding save path (use .h5 extension).")
 
     argparser.add_argument("--detection", type=str, metavar="image_path",
-                           help="Returns bodies of the given image in the specified folder using --saveFolder option. Default body detector: MaskRCNN")
+                           help="Returns bodies of the given image in the specified folder using --saveFolder option. Default body detector: MaskRCNN.")
     argparser.add_argument("--mtcnn", action='store_true', help="Uses MTCNN for face detection.")
     argparser.add_argument("--yolo", action='store_true', help="Uses YOLOv3 for person detection.")
     argparser.add_argument("--faster", action='store_true', help="Uses FasterRCNN for person detection.")
@@ -29,8 +31,8 @@ def buildArgumentParser():
     argparser.add_argument("--bodyDetectorConfig", type=str, metavar="configuration_file_path",
                            help="Body detector configuration file. Only needed for YOLOv3.")
     argparser.add_argument("--detectionThreshold", type=float, metavar="threshold_float",
-                           help="Specifies the detection threshold.")
-    argparser.add_argument("--boundingBox", action='store_true', help="Draws bounding boxes in image")
+                           help="Specifies the detection threshold. Default Value: 0.8.")
+    argparser.add_argument("--boundingBox", action='store_true', help="Draws bounding boxes in image.")
     argparser.add_argument("--saveFolder", type=str, metavar="folder_path",
                            help="Specifies the save folder for the --detection option.")
 
@@ -53,16 +55,16 @@ def buildArgumentParser():
                            help="Display the CMC curve using time heuristic of the given query and gallery.")
     argparser.add_argument("--raceRanking", type=str, metavar="rank_file_path",
                            help="Previous race ranking file for CMC curve for heuristics.")
-    argparser.add_argument("--windowSize", type=int, metavar="integer", help="Window size for time heuristic.")
-    argparser.add_argument("--shiftSize", type=int, metavar="integer", help="Shift size for time heuristic.")
-    argparser.add_argument("--shiftProp", type=float, metavar="float", help="Shift proportion for time heuristic.")
+    argparser.add_argument("--windowSize", type=int, metavar="integer", help="Window size for time heuristic. Default value: 30.")
+    argparser.add_argument("--shiftSize", type=int, metavar="integer", help="Shift size for time heuristic. Default value: 1.")
+    argparser.add_argument("--shiftProp", type=float, metavar="float", help="Shift proportion for time heuristic. Default value: 1.")
 
     argparser.add_argument("--spaceHeuristic", action='store_true',
                            help="Display the CMC curve using space heuristic of the given query and gallery.")
-    argparser.add_argument("--fps", type=int, metavar="integer", help="FPS for space heuristic.")
-    argparser.add_argument("--timeout", type=int, metavar="float", help="Timeout for time heuristic.")
+    argparser.add_argument("--fps", type=int, metavar="integer", help="FPS for space heuristic. Default value: 5.")
+    argparser.add_argument("--timeout", type=int, metavar="float", help="Timeout for space heuristic. Default value: 10.")
 
-    argparser.add_argument("--video", type=str, help="Runs system with specified file video.")
+    argparser.add_argument("--video", type=str, metavar="video_path", help="Runs system with specified video file.")
     argparser.add_argument("--camera", action='store_true', help="Runs system with system camera.")
     argparser.add_argument("--openWorldThreshold", type=float, metavar="float",
                            help="The minimum value to consider an identity as equal as one in the gallery, used in the open world assumption.")
